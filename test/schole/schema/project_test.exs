a264465e8ed39be3d72fbeaf69e1257b8f6b6567
lucperkins <lucperkins@gmail.com>
2020-04-11 22:33:38 -0700

    More unit tests for Project schema

--- test/schole/schema/project_test.exs
+++ test/schole/schema/project_test.exs
@@ -3,11 +3,24 @@ defmodule Schole.Schema.ProjectTest do
 
   alias Schole.Projects.Project
 
-  @valid %{title: "Some title", slug: "some-slug", metadata: %{foo: "bar", baz: 100, bop: [10]}}
-  @invalid_no_title %{slug: "slug-only"}
+  @meta %{foo: "bar", baz: 100, bop: [10]}
+  @valid %{title: "Some title", slug: "some-slug", metadata: @meta}
+  @valid_no_slug %{title: "Some title", metadata: @meta}
+  @valid_no_meta %{title: "Some title"}
+  @invalid_no_title %{slug: "slug-only", metadata: @meta}
 
   test "valid parameters" do
     changeset = create_changeset(@valid)
+    expect_valid(changeset)
+  end
+
+  test "valid no slug" do
+    changeset = create_changeset(@valid_no_slug)
+    expect_valid(changeset)
+  end
+
+  test "valid no metadata" do
+    changeset = create_changeset(@valid_no_meta)
     expect_valid(changeset)
   end
 
