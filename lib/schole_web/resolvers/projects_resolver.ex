80523a29a437170510ffb095f96977bb1ef96819
lucperkins <lucperkins@gmail.com>
2020-04-12 11:32:46 -0700

    Extract error wrapping logic out of ProjectsResolver

--- lib/schole_web/resolvers/projects_resolver.ex
+++ lib/schole_web/resolvers/projects_resolver.ex
@@ -1,6 +1,7 @@
 defmodule ScholeWeb.Resolvers.ProjectsResolver do
   alias Ecto.Changeset
   alias Schole.Projects
+  alias ScholeWeb.Resolvers.Helpers
 
   def all(_parent, _args, _resolution) do
     projects = Projects.all
@@ -16,27 +17,6 @@ defmodule ScholeWeb.Resolvers.ProjectsResolver do
   end
 
   def create(_parent, args, _resolution) do
-    wrapped_call(fn -> Projects.create(args) end)
-  end
-
-  defp format_errors(changeset) do
-    errors = Changeset.traverse_errors(changeset, fn {msg, opts} ->
-      Enum.reduce(opts, msg, fn {k, v}, formatted_msg ->
-        formatted_msg |> String.replace("%{#{k}}", to_string(v))
-      end)
-    end)
-
-    %{
-      message: "Validation errors occurred",
-      code: :schema_errors,
-      errors: errors
-    }
-  end
-
-  defp wrapped_call(fun) do
-    case fun.() do
-      {:ok, result} -> {:ok, result}
-      {:error, %Changeset{} = changeset} -> {:error, format_errors(changeset)}
-    end
+    Helpers.wrapped_call(fn -> Projects.create(args) end)
   end
 end
