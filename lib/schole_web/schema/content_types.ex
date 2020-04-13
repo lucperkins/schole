c42486718676632425b4c5d30cd4d40d566139bb
lucperkins <lucperkins@gmail.com>
2020-04-12 21:57:57 -0700

    Add association fields for Project and Document types

--- lib/schole_web/schema/content_types.ex
+++ lib/schole_web/schema/content_types.ex
@@ -1,12 +1,23 @@
 defmodule ScholeWeb.Schema.ContentTypes do
   use Absinthe.Schema.Notation
   import_types ScholeWeb.Schema.JSONScalar
+  alias Schole.Repo
 
   object :project do
     field :id, :id
     field :slug, :string
     field :title, :string
     field :metadata, :json
+    field :documents, list_of(:document) do
+      resolve fn project, _, _ ->
+        documents =
+          project
+          |> Ecto.assoc(:documents)
+          |> Repo.all()
+
+        {:ok, documents}
+      end
+    end
   end
 
   object :document do
@@ -16,5 +27,14 @@ defmodule ScholeWeb.Schema.ContentTypes do
     field :content, :string
     field :metadata, :json
     field :tags, list_of(:string)
+    field :project, :project do
+      resolve fn document, _, _ ->
+        project =
+          document
+          |> Ecto.assoc(:project)
+          |> Repo.one()
+        {:ok, project}
+      end
+    end
   end
 end
