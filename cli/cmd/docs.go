f96fe8c18c1720c470f80aa032f79a976cfafc15
lucperkins <lucperkins@gmail.com>
2020-05-02 18:43:40 -0700

    Refine document processing functions

--- cli/cmd/docs.go
+++ cli/cmd/docs.go
@@ -3,17 +3,31 @@ package cmd
 import (
 	"bytes"
 	"fmt"
+	"github.com/gernest/front"
 	"github.com/mitchellh/mapstructure"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
-	"github.com/yuin/goldmark"
-	meta "github.com/yuin/goldmark-meta"
-	"github.com/yuin/goldmark/parser"
 	"io/ioutil"
 	"os"
 	"path/filepath"
 	"strings"
 )
+
+var allowedExtensions = []string{".md"}
+
+type Document struct {
+	Url         string
+	Title       string
+	Description string
+	Tags        []string
+	Content     string
+}
+
+type Metadata struct {
+	Title       string
+	Description string
+	Tags        []string
+}
 
 func docsCmd(v *viper.Viper) *cobra.Command {
 	cmd := &cobra.Command{
@@ -36,95 +50,121 @@ func syncDocs(v *viper.Viper) *cobra.Command {
 		Use:   "sync",
 		Short: "Sync the local docs directory with the Schole API",
 		Run: func(_ *cobra.Command, _ []string) {
-			syncDirectory(dir)
+			errs := syncDirectory(dir)
+
+			if len(errs) > 0 {
+				fmt.Println("multiple errors")
+			} else {
+				fmt.Println("docs successfully synced")
+			}
 		},
 	}
 
 	return cmd
 }
 
-type File struct {
-	Url         string
-	Title       string
-	Description string
-	Tags        []string
-	Content     string
-}
+func syncDirectory(dir string) []error {
+	errs := make([]error, 0)
 
-type Metadata struct {
-	Title       string
-	Description string
-	Tags        []string
-}
+	exitOnError(filepath.Walk(dir, func(path string, info os.FileInfo, _ error) error {
+		if !info.IsDir() && hasAllowedExtension(path) {
+			doc, err := parseFile(path)
+			exitOnError(err)
 
-func syncDirectory(dir string) {
-	err := filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
-		if !info.IsDir() {
-			var url string
-
-			if strings.HasPrefix(path, "/") {
-				url = path
-			} else {
-				url = fmt.Sprintf("/%s", path)
-			}
-
-			bs, err := ioutil.ReadFile(path)
-			if err != nil {
-				return err
-			}
-
-			metadata, err := getMetadata(bs)
-			if err != nil {
-				return err
-			}
-
-			var m Metadata
-
-			if err := mapstructure.Decode(metadata, &m); err != nil {
-				return err
-			}
-
-			f := File{
-				Url:     url,
-				Content: string(bs),
-			}
-
-			if err := applyMetadataToFile(&f, &m, path); err != nil {
-				return err
-			}
-
-			return nil
+			fmt.Println(doc.Tags)
 		}
 
 		return nil
-	})
-	exitOnError(err)
+	}))
+
+	return errs
 }
 
-func getMetadata(bs []byte) (map[string]interface{}, error) {
-	md := goldmark.New(goldmark.WithExtensions(meta.Meta))
+func parseFile(path string) (*Document, error) {
+	var meta Metadata
 
-	var buf bytes.Buffer
-	ctx := parser.NewContext()
-	if err := md.Convert(bs, &buf, parser.WithContext(ctx)); err != nil {
+	// Get raw file data
+	file, err := ioutil.ReadFile(path)
+	if err != nil {
 		return nil, err
 	}
 
-	return meta.Get(ctx), nil
+	// Ensure that the file has a front matter delimiter ("---") at the top
+	if !strings.HasPrefix(strings.TrimSpace(string(file)), "---") {
+		return nil, fmt.Errorf("no document metadata supplied in %s", path)
+	}
+
+	// Extract front matter and content
+	m := front.NewMatter()
+	m.Handle("---", front.YAMLHandler)
+	f, body, err := m.Parse(bytes.NewReader(file))
+	if err != nil {
+		return nil, err
+	}
+
+	// Ensure that front matter has been supplied
+	if f == nil {
+		return nil, fmt.Errorf("no document metadata supplied in %s", path)
+	}
+
+	if err := mapstructure.Decode(f, &meta); err != nil {
+		return nil, err
+	}
+
+	if err := validateMetadata(&meta, path); err != nil {
+		return nil, err
+	}
+
+	doc := Document{
+		Url: getUrl(path),
+		Content: body,
+	}
+
+	applyMetadataToDoc(&doc, &meta)
+
+	return &doc, nil
 }
 
-func applyMetadataToFile(f *File, m *Metadata, path string) error {
+func applyMetadataToDoc(doc *Document, meta *Metadata) {
+	doc.Title = meta.Title
+
+	if meta.Tags != nil {
+		doc.Tags = meta.Tags
+	}
+
+	if meta.Description != "" {
+		doc.Description = meta.Description
+	}
+}
+
+func validateMetadata(m *Metadata, path string) error {
 	if m.Title == "" {
-		return fmt.Errorf("no title provided in %s", path)
-	}
-
-	if m.Description != "" {
-		f.Description = m.Description
-	}
-
-	if m.Tags != nil {
-		f.Tags = m.Tags
+		return fmt.Errorf("no title provided in file metadata in %s", path)
 	}
 
 	return nil
+}
+
+func getUrl(path string) string {
+	if !strings.HasPrefix(path, "/") {
+		path = fmt.Sprintf("/%s", path)
+	}
+
+	return path
+}
+
+func hasAllowedExtension(path string) bool {
+	ext := filepath.Ext(path)
+
+	return contains(allowedExtensions, ext)
+}
+
+func contains(ss []string, s string) bool {
+	for _, i := range ss {
+		if i == s {
+			return true
+		}
+	}
+
+	return false
 }
--- cli/go.mod
+++ cli/go.mod
@@ -3,11 +3,10 @@ module cli
 go 1.14
 
 require (
+	github.com/gernest/front v0.0.0-20181129160812-ed80ca338b88
 	github.com/mitchellh/mapstructure v1.3.0
 	github.com/spf13/cobra v1.0.0
 	github.com/spf13/pflag v1.0.3
 	github.com/spf13/viper v1.4.0
-	github.com/yuin/goldmark v1.1.7
-	github.com/yuin/goldmark-meta v0.0.0-20191126180153-f0638e958b60
 	gopkg.in/yaml.v2 v2.2.8 // indirect
 )
