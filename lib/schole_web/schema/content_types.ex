043f8e5ddb4d4e14216688df5d1adbb3f8f097c7
lucperkins <lucperkins@gmail.com>
2020-05-31 13:48:28 -0700

    Remove unused types from GraphQL interfaces

--- lib/schole_web/schema/content_types.ex
+++ lib/schole_web/schema/content_types.ex
@@ -3,49 +3,6 @@ defmodule ScholeWeb.Schema.ContentTypes do
 
   use Absinthe.Schema.Notation
   import_types(ScholeWeb.Schema.JSONScalar)
-  alias Schole.Repo
-
-  @desc "A Schole documentation project"
-  object :project do
-    @desc "The project's unique identifier"
-    field :id, non_null(:id)
-
-    @desc "The unique slug for the project, e.g. my-docs-project"
-    field :slug, non_null(:string)
-
-    @desc "The unique title for the project, e.g. 'My Docs Project'"
-    field :title, non_null(:string)
-
-    @desc "Key-value metadata associated with the project"
-    field :metadata, :json
-
-    @desc "Descriptive tags for indexing the project"
-    field :tags, list_of(:string)
-
-    @desc "The documents associated with the project"
-    field :documents, list_of(:document) do
-      resolve(fn project, _, _ ->
-        documents =
-          project
-          |> Ecto.assoc(:documents)
-          |> Repo.all()
-
-        {:ok, documents}
-      end)
-    end
-
-    @desc "Release notes associated with the project"
-    field :release_notes, list_of(:release_note) do
-      resolve(fn project, _, _ ->
-        release_notes =
-          project
-          |> Ecto.assoc(:release_notes)
-          |> Repo.all()
-
-        {:ok, release_notes}
-      end)
-    end
-  end
 
   @desc "The core document type for Schole documentation"
   object :document do
@@ -69,48 +26,6 @@ defmodule ScholeWeb.Schema.ContentTypes do
 
     @desc "Descriptive tags for indexing the document"
     field :tags, list_of(:string)
-
-    @desc "The project under which the document exists"
-    field :project, :project do
-      resolve(fn document, _, _ ->
-        project =
-          document
-          |> Ecto.assoc(:project)
-          |> Repo.one()
-
-        {:ok, project}
-      end)
-    end
-  end
-
-  @desc "Version-specific notes for the Schole project"
-  object :release_note do
-    @desc "The release notes' unique identifier"
-    field :id, non_null(:id)
-
-    @desc "The project version associated with the release notes"
-    field :version, non_null(:string)
-
-    @desc "The notes content for this version of the project"
-    field :notes, non_null(:string)
-
-    @desc "The project under which the release notes exist"
-    field :project, :project do
-      resolve(fn release_note, _, _ ->
-        project =
-          release_note
-          |> Ecto.assoc(:project)
-          |> Repo.one()
-
-        {:ok, project}
-      end)
-    end
-  end
-
-  input_object :new_project do
-    field :title, non_null(:string)
-    field :slug, :string
-    field :metadata, :json
   end
 
   input_object :new_document do
@@ -120,10 +35,5 @@ defmodule ScholeWeb.Schema.ContentTypes do
     field :content, non_null(:string)
     field :metadata, :json
     field :tags, list_of(:string)
-  end
-
-  input_object :new_release_note do
-    field :version, non_null(:string)
-    field :notes, non_null(:string)
   end
 end
